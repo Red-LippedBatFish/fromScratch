d933a89344fa331cec7c1eccb32a411f36c783c4
Lucy Chi <lucycchi@gmail.com>
2020-07-25 17:10:05 -0700

    homepage and modal initial render done, need functionality

--- src/client/components/homepage.jsx
+++ src/client/components/homepage.jsx
@@ -1,27 +1,36 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import { AppBar } from '@material-ui/core';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { AppBar } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
 
+import { Typography } from "@material-ui/core";
+import LoginModal from "./loginModal";
 
-const Homepage= ()=>{
-    return //components
-    (<div>
-        <AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" className={classes.title}>
-      Logo
-    </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-</AppBar>
+//add modal pop up as a component
+
+const Homepage = () => {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          ></IconButton>
+          <Typography variant="h6">Logo</Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+
+      <Button className="modal-button" variant="contained" color="primary">
+        Sign Up
+      </Button>
+      <LoginModal />
     </div>
-
-    )
-}
-
-
+  );
+};
 
 export default Homepage;
