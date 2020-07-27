8e676348574e2e4287589d300f4b1b48dafb8bd5
mjmiguel <migueljmiller@gmail.com>
2020-07-27 15:36:02 -0700

    add getProjects

--- src/client/App.js
+++ src/client/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./app.scss";
 import Homepage from "./components/homepage";
-
+import Main from './components/main'
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Scratch Project!?</h1>
-        <Homepage />
+        <Main />
       </div>
     );
   }
